39e43904323511da6b3e93610f60e15e9c58c6ac
Erkhem <erkhem.unenbat@gmail.com>
2025-07-05 17:11:28 -0700

    going to restructure to multifile

--- init.lua
+++ init.lua
@@ -222,6 +222,27 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+if vim.g.neovide then
+  -- Helper function for transparency formatting
+  local alpha = function()
+    return string.format('%x', math.floor(255 * vim.g.transparency))
+  end
+  -- g:neovide_opacity should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_opacity = 0.9
+  vim.g.transparency = 0.95
+  vim.g.neovide_background_color = '#0f1117' .. alpha()
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_confirm_quit = true
+  -- vim.g.neovide_macos_simple_fullscreen = true
+
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_cursor_animation_length = 0.150
+
+  vim.g.neovide_cursor_short_animation_length = 0.04
+
+  vim.g.neovide_cursor_trail_size = 1.0
+end
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
